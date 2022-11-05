6dda6895b7599984f51d2f83758e503660723167
Qiameth <karenmurgana@gmail.com>
2022-11-04 22:47:34 -0300

    cambios a todo

--- js/ticket.js
+++ js/ticket.js
@@ -1,45 +1,122 @@
+function captura() {
+  var nombre = document.getElementById("inputNombre").value;
+  var apellido = document.getElementById("inputApellido").value;
+  var correo = document.getElementById("inputCorreo").value;
+  var cantidad = document.getElementById("inputCantidadEntradas");
+  var cantidadValue = document.getElementById("inputCantidadEntradas").value;
+  var categoriaValue = document.getElementById("categoriaEntrada").value;
+  var totalPagar = calcularPrecio(categoriaValue, cantidad);
+
+  const labelNombre = document.getElementById("etiquetaNombre");
+  const labelApellido = document.getElementById("etiquetaApellido");
+  const labelCorreo = document.getElementById("etiquetaCorreo");
+  const labelCantidad = document.getElementById("etiquetaCantidad");
+  const labelCategoria = document.getElementById("etiquetaCategoria");
+  const labelPrecioTotal = document.getElementById("etiquetaTotal");
+
+  labelNombre.innerHTML = nombre;
+  labelApellido.innerHTML = apellido;
+  labelCorreo.innerHTML = correo;
+  labelCantidad.innerHTML = cantidadValue;
+  labelPrecioTotal.innerHTML = "$" + totalPagar;
+
+  switch (categoriaValue) {
+    case "1":
+      labelCategoria.innerHTML = "Estudiante";
+      break;
+    case "2":
+      labelCategoria.innerHTML = "Trainee";
+      break;
+    case "3":
+      labelCategoria.innerHTML = "Junior";
+      break;
+    case "4":
+      labelCategoria.innerHTML = "General";
+      break;
+    default:
+      labelCategoria.innerHTML = "General";
+      break;
+  }
+}
+
+function esconder() {
+  var x = document.getElementById("sectionResumen");
+  var y = document.getElementById("sectionDatos");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+
+    y.style.display = "block";
+  }
+}
+
+/**Variables capturadas del form */
+const labelTotal = document.getElementById("precioTotal");
+
+
 /**Calcula el precio onchange */
 function calcularPrecio(categ, cant) {
-  const labelTotal = document.getElementById("precioTotal");
-
   let cantidadEntradas = cant.value;
   let precioTotal = 200;
   let precioSinDescuento = precioTotal * cantidadEntradas;
-
-  switch (categ) {
-    case "1":
-      precioTotal *= 0.2;
-      break;
-    case "2":
-      precioTotal *= 0.5;
-      break;
-    case "3":
-      precioTotal *= 0.85;
-      break;
-    case "4":
-      precioTotal *= 1;
-      precioSinDescuento = "";
-      break;
-  }
 
   if (cantidadEntradas < 0) {
     return;
   } else {
     precioTotal *= cantidadEntradas;
+
+    switch (categ) {
+      case "1":
+        precioTotal *= 0.2;
+        break;
+      case "2":
+        precioTotal *= 0.5;
+        break;
+      case "3":
+        precioTotal *= 0.85;
+        break;
+      case "4":
+        precioTotal *= 1;
+        precioSinDescuento = "";
+        break;
+      default:
+        precioTotal *= 1;
+        precioSinDescuento = "";
+        break;
+    }
+
     if (categ === "4") {
       labelTotal.innerHTML = precioTotal;
-      return;
     } else {
       labelTotal.innerHTML =
         precioTotal + "  \u00A0$" + ("<del>" + precioSinDescuento + "</del>");
     }
+    return precioTotal;
   }
 }
 
 /**Limpiar el campo total a pagar cuando se presione borrar*/
 function limpiarTotalPagar() {
-  const labelTotal = document.getElementById("precioTotal");
+  var getValue = document.getElementById("inputNombre");
+  const labelApel = document.getElementById("inputApellido");
+  const labelCor = document.getElementById("inputCorreo");
+  const labelCant = document.getElementById("inputCantidadEntradas");
+  const labelCat = document.getElementById("categoriaEntrada");
+
+  if (getValue.value != "") {
+    getValue.value = "";
+  }
+  labelNom.reset();
   labelTotal.innerHTML = "";
+  labelApel.innerHTML = "";
+  labelCat.innerHTML = "";
+  labelCant.innerHTML = "";
+  labelCor.innerHTML = "";
+  
 }
 
 /**Abrir ventana de resumen de compra */
@@ -50,8 +127,11 @@ function abrirVentana() {
     "resumenCompra",
     caracVentana
   );
+
   if (!handle) {
     window.alert("No se pudo abrir la ventana debido al bloqueo de popups");
+  } else {
+    datosResumen();
   }
 }
 
@@ -59,11 +139,3 @@ function abrirVentana() {
 function cerrarVentana() {
   window.close("resumenCompra.html");
 }
-
-/**Temporal, ARREGLAR!! ---- */
-function datosResumen() {
-  const labelNombre = document.getElementById("etiquetaNombre");
-  const labelTotal = document.getElementById("precioTotal");
-  labelNombre.innerHTML = "nombre provisorio";
-}
-datosResumen();
